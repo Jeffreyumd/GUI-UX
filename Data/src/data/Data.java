e4bacb36c03324db1aa9443434d0beb15b2c4b08
Jeffrey A <alhassan.jeffrey@yahoo.com>
2017-04-29 20:41:28 -0400

    update/test

--- Data/src/data/Data.java
+++ Data/src/data/Data.java
@@ -4,18 +4,71 @@
  * and open the template in the editor.
  */
 package data;
-
+import java.sql.*;
 /**
  *
  * @author Jeffrey A
  */
 public class Data {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+                
+      private static Connection myConn;
+      
+    public static void main(String[] args) throws Exception{
+        
+
+		String user = "root";
+		String password = "";
+		String url = "jdbc:mysql://localhost:3306/mail";
+                
+                //Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+		// establish database connection
+		myConn = DriverManager.getConnection(url, user, password);
+		
+		System.out.println("DB connection successful to:" + url);
+                
+                getAllcity();
     }
+    
+    	public static void getAllcity() throws Exception {
+		
+		//instance for ArrayList city
+		//List<City> list = new ArrayList<>();
+		
+		Statement sqlState = null;
+		ResultSet rows = null;
+		
+		try
+		{
+			sqlState = myConn.createStatement();
+			rows = sqlState.executeQuery("Select * from city");
+			
+			while(rows.next())
+			{	
+			 System.out.println(rows.getString("Name"));
+			}
+			
+			//return list;
+		} 
+                		finally
+		{
+			//close(sqlState, rows);
+		}
+        }
+        
+        private City createCity(ResultSet rows) throws SQLException{
+		
+		//store required parameters for city instance
+		int id = rows.getInt("ID");
+		int population = rows.getInt("Population");
+		String name = rows.getString("Name");
+		String district = rows.getString("District");
+		String cc = rows.getString("CountryCode");
+		
+		//instance of city
+		City tempCity = new City(id, name, cc, district, population);
+		
+		return tempCity;
+	}
     
 }
